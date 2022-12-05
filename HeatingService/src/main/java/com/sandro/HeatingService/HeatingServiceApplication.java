9dd96052ea7fe797da6e4cabe7f6b01e1c4c6ce4
pizterri <96497839+pizterri@users.noreply.github.com>
2022-12-05 20:11:58 +0100

    Initial Commit
    init

---
+++ HeatingService/src/main/java/com/sandro/HeatingService/HeatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sandro.HeatingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatingServiceApplication.class, args);
+	}
+
+}
---
+++ HeatingService/src/test/java/com/sandro/HeatingService/HeatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandro.HeatingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
